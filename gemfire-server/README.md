ab2c77417b1565b0c98df79ebe95d1ecb900703d
dwong-pivotal <derrick.chwong@gmail.com>
2017-06-02 15:44:52 +0800

    update readme for gemfire server and cleanup

--- gemfire-server/README.md
+++ gemfire-server/README.md
@@ -7,3 +7,4 @@ To build:
 
 To Run a Gemfire Server, please refer to the [sample_start_server_script.sh](sample_start_server_script.sh)
 
+To stop a server, simply ```kill -9 <PID of Gemfire Server>```
--- gemfire-server/sample_start_server_script.sh
+++ gemfire-server/sample_start_server_script.sh
@@ -3,4 +3,4 @@ export MYSQL_URI=<mysql uri>
 export MYSQL_USERNAME=<username>
 export MYSQL_PASSWORD=<password>
 export CACHE_SERVER_HOSTNAME=<gemfire hostname or IP>
-nohup java -Xmx4g -jar -Dgemfire.cache.server.hostname-for-clients=<gemfire hostname or IP> -Dspring.datasource.tomcat.max-active=20 -Dgemfire.pdx.disk-store-dir=/home/devops/server/server1 -Dgemfire.async-event.dispatcher-thread=5 -Dgemfire.async-event.batch-size=1000 -Dgemfire.cache.server.port=40404 -Dgemfire.server.name=DefaultGemfireServer1 -Dgemfire.manager.port=1096 ../*.jar &
+nohup java -Xmx4g -jar -Dgemfire.cache.server.hostname-for-clients=<gemfire hostname or IP> -Dspring.datasource.tomcat.max-active=20 -Dgemfire.pdx.disk-store-dir=/home/devops/server/server1 -Dgemfire.async-event.dispatcher-thread=5 -Dgemfire.async-event.batch-size=1000 -Dgemfire.cache.server.port=40404 -Dgemfire.server.name=DefaultGemfireServer1 -Dgemfire.manager.port=1096 target/demo-gemfire-server-0.0.1_SNAPSHOTjar &
